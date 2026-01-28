132006c02efe8efdee274703fc7a621adc337fd0
game\242j005 <ttno6018@gmail.com>
2026-01-28 14:24:44 +0900

    プレイヤーが鬼に触れたとき、牢屋の位置までテレポートできるようになりました。

--- OgurasamaOnline/Assets/takahashi/SharedModeScript/JailTeleport.cs
+++ OgurasamaOnline/Assets/takahashi/SharedModeScript/JailTeleport.cs
@@ -6,12 +6,23 @@ public class JailTeleport : NetworkBehaviour
     //牢屋の座標
     private Transform jailTransform;
 
+    //牢屋に捕まったかどうかの判定
     private bool isCaught = false;
+
+    private NetworkCharacterController networkCharacterController;
 
     public override void Spawned()
     {
-        //JailManagerから牢屋のTransformを取得
+        //Debug.Log($"JailManager.Instance = {JailManager.Instance}");
+
+        //牢屋のTransformを取得
+        //staticを使ったクラスであるためクラス名を書いただけでアクセス可能
         jailTransform = JailManager.Instance.jailTransform;
+
+        //NetworkCharacterControllerがプレイヤーの子オブジェクトにあるためGetComponentInChildrenで取得する
+        networkCharacterController = GetComponentInChildren<NetworkCharacterController>();
+
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,7 +32,8 @@ public class JailTeleport : NetworkBehaviour
             return;
         }
 
-        if(other.CompareTag("Enemy"))
+        //敵に触れたら牢屋にテレポートするRPCを呼び出す
+        if (other.CompareTag("Enemy"))
         {
             //Debug.Log("hit");
             RPC_RequestJail();
@@ -30,7 +42,9 @@ public class JailTeleport : NetworkBehaviour
     }
 
     /// <summary>
-    /// 
+    /// HasStateAuthority側で実行するRPCメソッド
+    /// オブジェクト権限が持っているものしか実行できなく、
+    /// Fusionでの唯一の位置変更できる方法
     /// </summary>
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
     private void RPC_RequestJail()
@@ -47,18 +61,48 @@ public class JailTeleport : NetworkBehaviour
     }
 
     /// <summary>
+    /// 牢屋の座標を確保するメソッド
+    /// </summary>
+    private void EnsureJailTransform()
+    {
+        if (jailTransform != null) return;
+
+        if (JailManager.Instance != null)
+            jailTransform = JailManager.Instance.jailTransform;
+    }
+
+    /// <summary>
     /// プレイヤーを牢屋にテレポートするメソッド
     /// </summary>
     public void TeleportToJail()
     {
+        
+        Debug.Log("メソッド呼び出し");
         //StateAuthorityを持っている場合のみ処理を行う
         //このオブジェクトが状態を変更する権限があるか確認
         if (!Object.HasStateAuthority)
         {
+            Debug.Log("権限なし");
+            return;
+        }
+
+        EnsureJailTransform();
+
+        if (networkCharacterController == null)
+        {
+            Debug.LogError("NetworkCharacterControllerが見つからない！");
+            return;
+        }
+
+        if (jailTransform == null)
+        {
+            Debug.LogError("jailTransformが取得できていない！");
             return;
         }
 
         //プレイヤーを牢屋の位置にテレポートさせる
-        transform.position = jailTransform.position;
+        //CharacterControllerで物理的な当たり判定、NetworkCharacterController
+        //でネットワーク同期と移動計算
+        networkCharacterController.Teleport(jailTransform.position);
     }
 }
